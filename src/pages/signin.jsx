da6d437c925591052cd2a317cd93253a6a9c0aed
Jay Bien-aimé <jay.bienaime@gmail.com>
2021-12-19 17:33:38 -0500

    signin page dark mode

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -61,7 +61,7 @@ localStorage && localStorage.setItem('aux', aux);
     <Main
       meta={
         <Meta
-          title="DAPP Signup"
+          title="DAP Signup"
           description=""
         />
       }
@@ -104,28 +104,33 @@ localStorage && localStorage.setItem('aux', aux);
       <Link
         href="/signup"
       >
-          Signup instead
+        <a className='ml-2'>
+        Signup instead
+
+        </a>
         </Link>
     </p>
   </div>
 
   <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-    <div className="bg-white py-8 px-6 shadow rounded-lg sm:px-10">
+    <div className="bg-white py-8 px-6 shadow rounded-lg sm:px-10
+    dark:bg-gray-900">
       <form className="mb-0 space-y-6" action="#" method="POST" onSubmit={ onLogin }>
         <div>
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email address</label>
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700 dark:text-gray-400">Email address</label>
           <div className="mt-1">
             <input 
             id="email" name="email" type="email" autoComplete="email" required 
             className="w-full border border-grey-400 px-3 py-2 rounded-lg shadow-sm
-            focus:outline-none focus:border-indigo-500" 
+            focus:outline-none focus:border-indigo-500
+            dark:bg-gray-600 dark:text-white" 
             onChange={ onChange }
             />
           </div>
         </div>
 
         <div>
-          <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+          <label htmlFor="password" className="block text-sm font-medium text-gray-700 dark:text-gray-400">Password</label>
           <div className="mt-1">
             <input 
             className="w-full border border-grey-400 px-3 py-2 rounded-lg shadow-sm

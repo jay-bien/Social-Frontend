2f25dbb39075272b1dfbf47bb35e6f3df579a342
Jay Bien-aimé <jay.bienaime@gmail.com>
2021-12-30 22:28:44 -0500

    removed cookie package also causing build to break

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,11 @@
 import { AppProps } from 'next/app';
 
 import '../styles/main.scss';
-import { CookiesProvider } from "react-cookie"
 
 
 
 const MyApp = ({ Component, pageProps }) => (
-  <CookiesProvider>
   <Component {...pageProps} />
-  </CookiesProvider>
 );
 
 export default MyApp;
